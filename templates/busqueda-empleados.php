6718e76bda51b95d47b16f30625103f4b2de97d4
Patricdb <patrisanchez.cdb@gmail.com>
2025-08-01 19:47:05 +0200

    Fix Spanish strings

--- templates/busqueda-empleados.php
+++ templates/busqueda-empleados.php
@@ -13,11 +13,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="cdb-busqueda-empleados">
     <div class="cdb-busqueda-filtros">
         <input type="text" id="cdb-nombre" placeholder="<?php esc_attr_e('Nombre','cdb-form'); ?>" />
-        <input type="text" id="cdb-posicion" placeholder="<?php esc_attr_e('Posici\xC3\xB3n','cdb-form'); ?>" />
+        <input type="text" id="cdb-posicion" placeholder="<?php esc_attr_e('Posición','cdb-form'); ?>" />
         <input type="hidden" id="cdb-posicion-id" />
         <input type="text" id="cdb-bar" placeholder="<?php esc_attr_e('Bar','cdb-form'); ?>" />
         <input type="hidden" id="cdb-bar-id" />
-        <input type="text" id="cdb-anio" placeholder="<?php esc_attr_e('A\xC3\xB1o','cdb-form'); ?>" />
+        <input type="text" id="cdb-anio" placeholder="<?php esc_attr_e('Año','cdb-form'); ?>" />
     </div>
     <div id="cdb-busqueda-empleados-resultados">
         <?php include CDB_FORM_PATH . 'templates/busqueda-empleados-table.php'; ?>
