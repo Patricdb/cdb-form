2ee3486d8d2880be7f120f83037d58c06eedeeea
Patricdb <patrisanchez.cdb@gmail.com>
2025-08-01 20:45:03 +0200

    Register frontend script and enqueue conditionally

--- public/enqueue.php
+++ public/enqueue.php
@@ -6,13 +6,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Cargar estilos y scripts en el frontend
 function cdb_form_public_enqueue() {
-    // Scripts
-    wp_enqueue_script( 'cdb-form-frontend-script', CDB_FORM_URL . 'assets/js/frontend-scripts.js', array( 'jquery' ), '1.0', true );
+    // Registrar el script sin encolarlo
+    wp_register_script(
+        'cdb-form-frontend-script',
+        CDB_FORM_URL . 'assets/js/frontend-scripts.js',
+        array( 'jquery' ),
+        '1.0',
+        true
+    );
 
     // Pasar AJAX URL y Nonce a JavaScript
     wp_localize_script( 'cdb-form-frontend-script', 'cdb_form_ajax', array(
-        'ajaxurl' => admin_url('admin-ajax.php'),
-        'nonce'   => wp_create_nonce('cdb_form_nonce') // Agregamos el nonce
-    ));
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'nonce'   => wp_create_nonce( 'cdb_form_nonce' ), // Agregamos el nonce
+    ) );
 }
 add_action( 'wp_enqueue_scripts', 'cdb_form_public_enqueue' );
